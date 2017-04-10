fa9d067856054ac287ecb588ab67fbb42cb303aa
Saubhik Mukherjee <saubhik.mukherjee@gmail.com>
2017-04-10 18:13:43 +0530

    Deleted wish.cpp

--- wish.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	cout << "I like to program and I would like to build stuff that can make the world a better place to live";
-	cout << "\n";
-	return 0;
-}
